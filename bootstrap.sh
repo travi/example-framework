3363be2f06714a300b18135ceb4d3e757bb704e8
Matt Travi <programmer@travi.org>
2014-01-27 21:48:33 -0600

    use librarian to install puppet modules as part of provisioning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+PUPPET_DIR='/vagrant/puppet'
+
 sudo apt-get update
+
+mkdir -p puppet/modules
+
+if [ `gem query --local | grep librarian-puppet | wc -l` -eq 0 ]; then
+  gem install librarian-puppet
+  cd $PUPPET_DIR && librarian-puppet install --clean
+else
+  cd $PUPPET_DIR && librarian-puppet update
+fi
