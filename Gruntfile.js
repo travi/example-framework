66818da289c96d984e2e94362b63dff108142421
Matt Travi <programmer@travi.org>
2014-01-27 22:06:19 -0600

    added aliases file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,4 @@ module.exports = function (grunt) {
     'use strict';
 
     require('load-grunt-config')(grunt);
-
-    grunt.registerTask('compile', ['sass']);
-    grunt.registerTask('build', ['bower', 'compile']);
-    grunt.registerTask('default', 'build');
 };
