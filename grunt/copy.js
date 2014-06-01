d402507a3a67aaf637a4ddf0a99fcb1f609c8a7b
Matt Travi <programmer@travi.org>
2014-06-01 11:33:27 -0500

    added a copy task so that the jquery-ui icons are available

---
+++ grunt/copy.js
@@ -0,0 +1,14 @@
+module.exports = {
+    framework: {
+        files: [
+            {
+                expand: true,
+                cwd: 'doc_root/resources/thirdparty/travi-styles/scss/icons/',
+                src: ['*'],
+                dest: 'doc_root/resources/thirdparty/travi-styles/css/icons/'
+            }
+
+        ]
+
+    }
+};
