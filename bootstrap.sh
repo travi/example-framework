bd31253f97b7c89bbcf23790457da756947ebe98
Matt Travi <programmer@travi.org>
2014-08-17 09:58:25 -0500

    removed directory creation that never worked anyway in favor of the step that has been in the Rakefile for a while now. closes #11

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,6 @@ PUPPET_DIR='/vagrant/puppet'
 
 sudo apt-get update
 
-mkdir -p puppet/modules
-
 if [ `librarian-puppet version 2> /dev/null | grep librarian-puppet | wc -l` -eq 0 ]; then
   apt-get install -y git librarian-puppet
   cd ${PUPPET_DIR} && librarian-puppet install --clean
