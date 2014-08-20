2526079a7628bab07c90092c12fa84a6db77f4d8
Matt Travi <programmer@travi.org>
2014-08-20 07:46:39 -0500

    error output can now access the class that it needs in order to determine how it should be reported

--- doc_root/index.php
+++ doc_root/index.php
@@ -1,6 +1,19 @@
 <?php
 
 use travi\framework\controller\front\FrontController;
+use travi\framework\utilities\Environment;
+
+
+date_default_timezone_set('America/Chicago');
+putenv("TZ=America/Chicago");
+
+require __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../config/db/db.conf';
+require_once __DIR__ . '/../vendor/travi/framework/php/framework/src/core/init.php';
+
+/** @var Environment $environment */
+$environment = $container->dependencies()->get('environment');
 
 ini_set('log_errors', 'On');
 if ($environment->isProduction()) {
@@ -12,14 +25,6 @@ if ($environment->isProduction()) {
     error_reporting(E_ALL ^ E_NOTICE);
     ini_set('display_errors', 'On');
 }
-
-date_default_timezone_set('America/Chicago');
-putenv("TZ=America/Chicago");
-
-require __DIR__ . '/../vendor/autoload.php';
-
-require_once __DIR__ . '/../config/db/db.conf';
-require_once __DIR__ . '/../vendor/travi/framework/php/framework/src/core/init.php';
 
 /** @var $frontController FrontController */
 $frontController = Pd_Make::name('travi\\framework\\controller\\front\\FrontController');
