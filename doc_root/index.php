0d550a6222f3d23abc71a18c36ba0618f5942365
Matt Travi <programmer@travi.org>
2014-08-19 20:58:10 -0500

    hides errors in prod, but still shows them locally. closes #17

--- doc_root/index.php
+++ doc_root/index.php
@@ -3,8 +3,15 @@
 use travi\framework\controller\front\FrontController;
 
 ini_set('log_errors', 'On');
-error_reporting(E_ALL ^ E_NOTICE);
-ini_set('display_errors', 'On');
+if ($environment->isProduction()) {
+    // Report all errors
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'Off');
+} else {
+    // Report all errors except E_NOTICE
+    error_reporting(E_ALL ^ E_NOTICE);
+    ini_set('display_errors', 'On');
+}
 
 date_default_timezone_set('America/Chicago');
 putenv("TZ=America/Chicago");
