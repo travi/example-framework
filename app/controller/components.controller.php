599063861dcb29e92fa458832a9c6c70fd739992
Matt Travi <programmer@travi.org>
2014-12-27 19:34:33 -0600

    moved form creation to a mapper

--- app/controller/components.controller.php
+++ app/controller/components.controller.php
@@ -1,29 +1,19 @@
 <?php
 
-use travi\framework\components\Forms\choices\CheckBoxes;
-use travi\framework\components\Forms\choices\RadioButtons;
-use travi\framework\components\Forms\choices\SelectionBox;
-use travi\framework\components\Forms\FieldSet;
-use travi\framework\components\Forms\Form;
-use travi\framework\components\Forms\inputs\DateInput;
-use travi\framework\components\Forms\inputs\FileInput;
-use travi\framework\components\Forms\inputs\NumberInput;
-use travi\framework\components\Forms\inputs\OpenIdInput;
-use travi\framework\components\Forms\inputs\PasswordInput;
-use travi\framework\components\Forms\inputs\RichTextArea;
-use travi\framework\components\Forms\inputs\TextArea;
-use travi\framework\components\Forms\inputs\TextInput;
-use travi\framework\components\Forms\SubmitButton;
-use travi\framework\content\collection\EntityBlock;
-use travi\framework\content\collection\EntityList;
+require_once __DIR__ . '/../mappers/FormMapper.php';
+
 use travi\framework\controller\AbstractController;
 use travi\framework\http\Request;
 use travi\framework\http\Response;
 
 class Components extends AbstractController
 {
-    private $config;
     const ENTITY_LIST_URL_PREFIX = '/components/entityList/';
+
+    private $config;
+
+    /** @var  FormMapper */
+    private $formMapper;
 
     /**
      * @param $request Request
@@ -61,140 +51,9 @@ class Components extends AbstractController
     public function forms(&$request, &$response)
     {
         $response->setTitle('Forms');
-
-        $form = new Form(array());
-
-        $inputs = new FieldSet();
-        $inputs->setLegend('Inputs');
-        $inputs->addFormElement(
-            new TextInput(
-                array(
-                    'label' => 'Text Input'
-                )
-            )
-        );
-        $inputs->addFormElement(
-            new DateInput(
-                array(
-                    'label' => 'Date Input'
-                )
-            )
-        );
-        $inputs->addFormElement(
-            new FileInput(
-                array(
-                    'label' => 'File Input'
-                )
-            )
-        );
-        $inputs->addFormElement(
-            new NumberInput(
-                array(
-                    'label' => 'Number Input'
-                )
-            )
-        );
-        $inputs->addFormElement(
-            new OpenIdInput(
-                array(
-                    'label' => 'OpenID Input'
-                )
-            )
-        );
-        $inputs->addFormElement(
-            new PasswordInput(
-                array(
-                    'label' => 'Password Input'
-                )
-            )
-        );
-
-        $textAreas = new FieldSet();
-        $textAreas->setLegend('Text Areas');
-        $textAreas->addFormElement(
-            new TextArea(
-                array(
-                    'label' => 'Text Area'
-                )
-            )
-        );
-        $textAreas->addFormElement(
-            new RichTextArea(
-                array(
-                    'label' => 'Rich Text Area'
-                )
-            )
-        );
-
-        $options = array(
-            array(
-                'label' => 'First Choice',
-                'value' => 1
-            ),
-            array(
-                'label' => 'Second Choice',
-                'value' => 2
-            )
-        );
-
-        $choices = new FieldSet();
-        $choices->setLegend('Choices');
-        $choices->addFormElement(
-            new SelectionBox(
-                array(
-                    'label' => 'Selection Box',
-                    'options' => $options
-                )
-            )
-        );
-        $choices->addFormElement(
-            new CheckBoxes(
-                array(
-                    'label' => 'Checkboxes',
-                    'options' => $options
-                )
-            )
-        );
-        $choices->addFormElement(
-            new RadioButtons(
-                array(
-                    'label' => 'Radio Buttons',
-                    'options' => $options
-                )
-            )
-        );
-        $choices->addFormElement(
-            new RadioButtons(
-                array(
-                    'label' => 'Inline',
-                    'options' => $options
-                )
-            )
-        );
-
-        $form->addFormElement($inputs);
-        $form->addFormElement($choices);
-        $form->addFormElement($textAreas);
-
-        $form->mapErrorMessagesToFields(
-            array(
-                'text_input' => 'This is an example error message',
-                'checkboxes' => 'This is an example error message for a choices group',
-                'inline' => 'This is an example error message for an inline choices group'
-            )
-        );
-
-        $form->addAction(
-            new SubmitButton(
-                array(
-                    'label' => 'Submit This Form'
-                )
-            )
-        );
-
         $response->setContent(
             array(
-                'form' => $form
+                'form' => $this->formMapper->createForm()
             )
         );
     }
@@ -206,5 +65,14 @@ class Components extends AbstractController
     public function setConfig($config)
     {
         $this->config = $config;
+    }
+
+    /**
+     * @PdInject $mapper new:FormMapper
+     * @param $mapper FormMapper
+     */
+    public function setFormMapper($mapper)
+    {
+        $this->formMapper = $mapper;
     }
 }
---
+++ app/mappers/FormMapper.php
@@ -0,0 +1,157 @@
+<?php
+
+use travi\framework\components\Forms\choices\CheckBoxes;
+use travi\framework\components\Forms\choices\RadioButtons;
+use travi\framework\components\Forms\choices\SelectionBox;
+use travi\framework\components\Forms\FieldSet;
+use travi\framework\components\Forms\Form;
+use travi\framework\components\Forms\inputs\DateInput;
+use travi\framework\components\Forms\inputs\FileInput;
+use travi\framework\components\Forms\inputs\NumberInput;
+use travi\framework\components\Forms\inputs\OpenIdInput;
+use travi\framework\components\Forms\inputs\PasswordInput;
+use travi\framework\components\Forms\inputs\RichTextArea;
+use travi\framework\components\Forms\inputs\TextArea;
+use travi\framework\components\Forms\inputs\TextInput;
+use travi\framework\components\Forms\SubmitButton;
+
+class FormMapper
+{
+
+    /**
+     * @return Form
+     */
+    public function createForm()
+    {
+        $form = new Form(array());
+
+        $inputs = new FieldSet();
+        $inputs->setLegend('Inputs');
+        $inputs->addFormElement(
+            new TextInput(
+                array(
+                    'label' => 'Text Input'
+                )
+            )
+        );
+        $inputs->addFormElement(
+            new DateInput(
+                array(
+                    'label' => 'Date Input'
+                )
+            )
+        );
+        $inputs->addFormElement(
+            new FileInput(
+                array(
+                    'label' => 'File Input'
+                )
+            )
+        );
+        $inputs->addFormElement(
+            new NumberInput(
+                array(
+                    'label' => 'Number Input'
+                )
+            )
+        );
+        $inputs->addFormElement(
+            new OpenIdInput(
+                array(
+                    'label' => 'OpenID Input'
+                )
+            )
+        );
+        $inputs->addFormElement(
+            new PasswordInput(
+                array(
+                    'label' => 'Password Input'
+                )
+            )
+        );
+
+        $textAreas = new FieldSet();
+        $textAreas->setLegend('Text Areas');
+        $textAreas->addFormElement(
+            new TextArea(
+                array(
+                    'label' => 'Text Area'
+                )
+            )
+        );
+        $textAreas->addFormElement(
+            new RichTextArea(
+                array(
+                    'label' => 'Rich Text Area'
+                )
+            )
+        );
+
+        $options = array(
+            array(
+                'label' => 'First Choice',
+                'value' => 1
+            ),
+            array(
+                'label' => 'Second Choice',
+                'value' => 2
+            )
+        );
+
+        $choices = new FieldSet();
+        $choices->setLegend('Choices');
+        $choices->addFormElement(
+            new SelectionBox(
+                array(
+                    'label' => 'Selection Box',
+                    'options' => $options
+                )
+            )
+        );
+        $choices->addFormElement(
+            new CheckBoxes(
+                array(
+                    'label' => 'Checkboxes',
+                    'options' => $options
+                )
+            )
+        );
+        $choices->addFormElement(
+            new RadioButtons(
+                array(
+                    'label' => 'Radio Buttons',
+                    'options' => $options
+                )
+            )
+        );
+        $choices->addFormElement(
+            new RadioButtons(
+                array(
+                    'label' => 'Inline',
+                    'options' => $options
+                )
+            )
+        );
+
+        $form->addFormElement($inputs);
+        $form->addFormElement($choices);
+        $form->addFormElement($textAreas);
+
+        $form->mapErrorMessagesToFields(
+            array(
+                'text_input' => 'This is an example error message',
+                'checkboxes' => 'This is an example error message for a choices group',
+                'inline' => 'This is an example error message for an inline choices group'
+            )
+        );
+
+        $form->addAction(
+            new SubmitButton(
+                array(
+                    'label' => 'Submit This Form'
+                )
+            )
+        );
+        return $form;
+    }
+}
